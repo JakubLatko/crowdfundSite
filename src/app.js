5e7cf469dfc3221f68966a6025b433f96918cac4
Jakub <jakub.latko@vp.pl>
2023-08-21 17:10:19 +0200

    mobile layout and mobile menu

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const navList = document.querySelector(".navList")
+const hamBtn = document.querySelector(".hamButton")
+const hamIcon = document.querySelector(".hamIcon")
+
+hamBtn.addEventListener("click", ()=>{
+    if(navList.ariaExpanded == "false"){
+        navList.classList.remove("navHidden")
+        navList.setAttribute("aria-expanded", "true")
+        hamIcon.src = "assets/images/icon-close-menu.svg"
+    }else if(navList.ariaExpanded == "true"){
+        navList.classList.add("navHidden")
+        navList.setAttribute("aria-expanded", "false")
+        hamIcon.src = "assets/images/icon-hamburger.svg"
+    }
+})
