7cc2d806aab1d68bc76df245c1ca0ad620ec1a45
Jakub <jakub.latko@vp.pl>
2023-08-24 17:19:28 +0200

    modals working

--- src/app.js
+++ src/app.js
@@ -19,15 +19,41 @@ hamBtn.addEventListener("click", ()=>{
 const mainModal = document.querySelector(".mainModal")
 const mainModalListeners = document.querySelectorAll(".mainModalListener")
 
+
+function mainModalSwitch(){
+    if(mainModal.ariaExpanded == "false"){
+        mainModal.classList.remove("modalHidden")
+        mainModal.setAttribute("aria-expanded", "true")
+    }else if(mainModal.ariaExpanded == "true"){
+        mainModal.classList.add("modalHidden")
+        mainModal.setAttribute("aria-expanded", "false")
+    }
+}
+
 mainModalListeners.forEach(element => {
-    element.addEventListener("click", ()=>{
-        console.log("dupa")
-        if(mainModal.ariaExpanded == "false"){
-            mainModal.classList.remove("modalHidden")
-            mainModal.setAttribute("aria-expanded", "true")
-        }else if(mainModal.ariaExpanded == "true"){
-            mainModal.classList.add("modalHidden")
-            mainModal.setAttribute("aria-expanded", "false")
-        }
-    })
+    element.addEventListener("click", mainModalSwitch)
 })
+
+
+const smallModal = document.querySelector(".smallModal")
+const smallModalClose = document.querySelector(".smallModalClose")
+
+
+function smallModalSwitch(){
+    if(smallModal.ariaExpanded == "false"){
+        smallModal.classList.remove("modalHidden")
+        smallModal.setAttribute("aria-expanded", "true")
+    }else if(smallModal.ariaExpanded == "true"){
+        smallModal.classList.add("modalHidden")
+        smallModal.setAttribute("aria-expanded", "false")
+    }
+}
+
+mainModal.addEventListener("submit", (e)=>{
+    mainModalSwitch()
+    e.preventDefault()
+    smallModalSwitch()
+})
+
+smallModalClose.addEventListener("click", smallModalSwitch)
+
